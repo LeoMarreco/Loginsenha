3cf63c2002d5b741b2cc74550a2a5cd1f7dfb54e
Leonardo Ferreira <silva.leonardoferreira@gmail.com>
2023-05-23 10:30:09 -0300

    Programa parcialmente pronto

--- main.py
+++ main.py
@@ -15,3 +15,12 @@ while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED:
         break
+    elif event == 'login':
+        senha_correta = '123456'
+        usuario_correto = 'leonardo'
+        usuario = values['usuario']
+        senha = values['senha']
+        if senha == senha_correta and usuario == usuario_correto:
+            window['mensagem'].update('Login feito com sucesso')
+        else:
+            window['mensagem'].update('Senha ou usiário incorreto')
