c3cc56e8f0603f35ba1a886953955403d2a42ae3
Leonardo Ferreira <silva.leonardoferreira@gmail.com>
2023-05-23 00:03:53 -0300

    Primeiro git

---
+++ __init__.py
@@ -0,0 +1,17 @@
+import PySimpleGUI as sg
+
+layout = [
+    [sg.Text('Usuário')],
+    [sg.Input(key='usuario')],
+    [sg.Texte('Senha')],
+    [sg.Input(key='senha')],
+    [sg.Button('login')],
+    [sg.Text('',key='mensagem')]
+]
+
+window = sg.Window('Login', Layout=layout)
+
+while True:
+    event, values = window.read
+    if event == sg.WIN_CLOSED:
+        break
