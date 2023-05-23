0d42cf60f57e5f636ae3b1f51f364a8df4e7cca7
Leonardo Ferreira <silva.leonardoferreira@gmail.com>
2023-05-23 00:34:29 -0300

    Programa funcionando

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-import PySimpleGui as sg
+import PySimpleGUI as sg
 
 layout = [
     [sg.Text('Usuário')],
     [sg.Input(key='usuario')],
-    [sg.Texte('Senha')],
+    [sg.Text('Senha')],
     [sg.Input(key='senha')],
     [sg.Button('login')],
     [sg.Text('',key='mensagem')],
 ]
 
-window = sg.Window('Login', Layout=layout)
+window = sg.Window('Login', layout)
 
 while True:
-    event, values = window.read
+    event, values = window.read()
     if event == sg.WIN_CLOSED:
         break
